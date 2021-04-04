886dccac3da41a3648038d461157450b29042781
Cameron Gorrie <cameronwgorrie@gmail.com>
2021-04-04 09:20:12 -0400

    Add help command

---
+++ commands/help.go
@@ -0,0 +1,41 @@
+package commands
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+type Help struct {
+	Commands map[string]Command
+}
+
+func (help *Help) Run(args []string) error {
+	if len(args) == 0 {
+		helpMessage()
+		return nil
+	}
+
+	cmd, ok := help.Commands[args[0]]
+	if !ok {
+		return errors.New("unrecognized command: " + args[0])
+	}
+	cmd.Usage()
+	return nil
+}
+
+func (help *Help) Usage() {
+	helpMessage()
+}
+
+func helpMessage() {
+	fmt.Fprintf(os.Stderr, `
+	scc [GLOBAL OPTIONS] COMMAND [COMMAND OPTIONS]
+	GLOBAL OPTIONS
+		-h			Remote scsynth host.
+		-p			Remote scsynth port.
+	COMMANDS
+		free		Free nodes.
+	For help with a particular command, "scc help COMMAND"
+	`)
+}
