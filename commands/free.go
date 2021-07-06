88ce4bb94d2ab99f28521cdcb7689c0699a1d92f
Cameron Gorrie <cameronwgorrie@gmail.com>
2021-07-05 20:25:54 -0400

    Free command supports multiple flags

--- commands/free.go
+++ commands/free.go
@@ -21,18 +21,21 @@ func (free *Free) Run(args []string) error {
 	fs := flag.NewFlagSet("free", flag.ContinueOnError)
 	fs.IntVar(&free.groupId, "group-id", 0, "group id")
 	fs.IntVar(&free.nodeId, "node-id", 0, "node id")
+
 	if err := fs.Parse(args[1:]); err != nil {
 		return err
 	}
 
-	switch {
-	case free.groupId != 0:
-		if err := free.client.FreeAll(int32(free.groupId)); err != nil {
-			return err
-		}
-	case free.nodeId != 0:
-		if err := free.client.NodeFree(int32(free.nodeId)); err != nil {
-			return err
+	for i := 0; i < len(args); i++ {
+		switch {
+		case free.groupId != 0:
+			if err := free.client.FreeAll(int32(free.groupId)); err != nil {
+				return err
+			}
+		case free.nodeId != 0:
+			if err := free.client.NodeFree(int32(free.nodeId)); err != nil {
+				return err
+			}
 		}
 	}
 
